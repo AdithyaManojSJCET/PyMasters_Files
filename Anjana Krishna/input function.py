391f0d2f406e4ed6904a3193ffd5242bdc77a02e
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-11-10 23:00:24 +0530

    Add files via upload

---
+++ Anjana Krishna/input function.py
@@ -0,0 +1,6 @@
+#Author Anjana KRishna
+#Date 10 0ct 2024
+# Program using input from user
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print(f"Hi{name}!.You are{age}years of age")
