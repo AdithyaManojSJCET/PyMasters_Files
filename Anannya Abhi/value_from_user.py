8f07248404ff14d5f68a32c557d54d82aab367a9
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-10 21:10:46 +0530

    Add files via upload

---
+++ Anannya Abhi/value_from_user.py
@@ -0,0 +1,9 @@
+'''
+Name: Anannnya Abhi
+Date: 10.11.24
+Program to get value from user using input() and print it
+'''
+name=input("Enter your name:")
+number1=int(input("Enter a number:"))
+number2=float(input("Enter a decimal number:"))
+print(f"Your name is:{name}\n First number is:{number1}\n Second number is:{number2}")
