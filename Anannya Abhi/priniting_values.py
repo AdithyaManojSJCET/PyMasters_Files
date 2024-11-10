d161a8df3b06dec3a948c6e851b1c4330ecb95cd
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-10 20:47:19 +0530

    Add files via upload

---
+++ Anannya Abhi/priniting_values.py
@@ -0,0 +1,11 @@
+'''
+Name: Anannnya Abhi
+Date: 10.11.24
+Program to print values using print()
+'''
+name="Anannya"
+number1=234
+number2=-77
+number3=12.76
+print(f"{name}\n{number1}\n{number2}\n{number3}")
+
