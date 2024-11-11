25f69306dee69dfaf94b851ec054a9f6c5c06ce6
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2024-11-11 20:41:42 +0530

    Add files via upload

---
+++ Adithya Manoj/eligibility to drive.py
@@ -0,0 +1,11 @@
+'''
+Author: Adithya Manoj
+Date: 11/11/2024
+Progran to know if you are eligible to drive
+'''
+age=int(input("Enter your age: "))
+
+if age >= 18:
+	print('You are eligible to drive!!')
+else:
+	print('You are not eligible to drive...')
