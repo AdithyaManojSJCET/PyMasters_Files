41aefc156248076d9f79ec6f9c9471c65cdfecd1
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-11-11 21:38:39 +0530

    Add files via upload

---
+++ Ann Maria Biju/ann1.py
@@ -0,0 +1,9 @@
+'''
+Name:ann mariya biju
+date:10/11/2024
+program to assigning variables with characters,integer and float values
+'''
+variable_1="how are you"
+variable_2=2345
+variable_3=0.87
+print(variable_1,"\n",variable_2,"\n",variable_3)
