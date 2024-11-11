e233284da73988c936110dd3afdddac0efded3e9
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-11-11 21:48:10 +0530

    Add files via upload

---
+++ Ann Maria Biju/ann2.py
@@ -0,0 +1,8 @@
+'''
+Name:ann mariya biju
+date:10/11/2024
+program to assigning variable using input() function and print the value
+'''
+variable_1=input("enter your name:")
+variable_2=int(input("enter your age:"))
+print(variable_1,"\n",variable_2)
