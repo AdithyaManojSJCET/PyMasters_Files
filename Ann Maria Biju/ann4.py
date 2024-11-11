f3057ebcda91307f9e851e8aef371e0576867f6c
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-11-11 22:26:45 +0530

    Add files via upload

---
+++ Ann Maria Biju/ann4.py
@@ -0,0 +1,10 @@
+'''
+Name:ann mariya biju
+date:10/11/2024
+program to perform to see if the user is old enough to get a drivings license
+'''
+age=int(input("Enter your age:"))
+if age>=18:
+    print("user is old enough to get a driving license" )
+else:
+    print("user is not old enough to get a driving license")
