4b7227ad83af2f569aa751d06e70518ee210ca05
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-11 22:08:08 +0530

    Add files via upload

---
+++ Anannya Abhi/licence.py
@@ -0,0 +1,11 @@
+'''
+Name: Anannnya Abhi
+Date: 11.11.24
+Python program to check eligibility for driver's licence
+'''
+age=int(input("Enter your age:"))  #modification needed
+if age>=18 and age<=80:
+    print("You are eligible for driver's licence!")
+else:
+    print("You are not eligible for driver's licence..")
+#float condition?
