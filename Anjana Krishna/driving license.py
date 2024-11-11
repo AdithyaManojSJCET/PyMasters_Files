f70a86af3237e0954660093c0f279e0c0bff44ff
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-11-12 00:31:28 +0530

    Add files via upload

---
+++ Anjana Krishna/driving license.py
@@ -0,0 +1,11 @@
+'''Author Anjana Krishna
+Date  11 oct 2024
+program to find eligibility to driving license '''
+
+age=int(input("Enter your age:"))
+
+if age>=18:
+    print("You are eligible")
+else:
+    print("You are not eligible")
+
