3e11ba8b35cabefbfbf7da9da92af1843f93f969
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-11-12 00:19:07 +0530

    Add files via upload

---
+++ Anjana Krishna/arithmetic operator.py
@@ -0,0 +1,21 @@
+'''Author Anjana Krishna
+Date  11 oct 2024
+program using arithmetic operators
+'''
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+
+sum=num1+num2#addition
+difference=num1-num2#subtraction
+product=num1*num2#multiplication
+division=num1/num2#division
+floor=num1//num2#division where ans is whole num
+expo=num1**num2#exponent
+
+print(f"Sum={sum}")
+print(f"Difference={difference}")
+print(f"Product={product}")
+print(f"Division={division}")
+print(f"floor division={floor}")
+print(f"exponent={expo}")
+
