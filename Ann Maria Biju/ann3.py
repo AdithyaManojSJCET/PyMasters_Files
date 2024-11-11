85cdd578ed43cfe532f7a786a764ec8db029394d
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-11-11 22:18:33 +0530

    Add files via upload

---
+++ Ann Maria Biju/ann3.py
@@ -0,0 +1,22 @@
+'''
+Name:ann mariya biju
+date:10/11/2024
+program to perform all arithemetic operations
+'''
+num1=int(input("enter number1:"))
+num2=int(input("enter number2:"))
+num3=num1+num2
+print("sum of number1 and number2 :",num3)
+num4=num2-num1
+print("difference of number1 and number 2:,",num4)
+num5=num1*num2
+print("product of number1 and number2:",num5)
+num6=num2/num1
+print("number2 divided by number 1:",num6)
+num7=num2//num1
+print("number2 divided by number 1:",num7)
+num8=num2%num1
+print("remainder when number2 is divided by number1:",num8)
+num9=(num1**num2)
+print(num9)
+
