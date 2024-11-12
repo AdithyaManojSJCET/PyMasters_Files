91140f829c5d627d7eea15254f3102de1de1758a
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-12 22:04:56 +0530

    Add files via upload

---
+++ Anannya Abhi/grade_mark.py
@@ -0,0 +1,14 @@
+'''
+Name: Anannnya Abhi
+Date: 12.11.24
+Python program to grade a student based on their marks
+'''
+mark=int(input("Enter your mark:"))
+if mark>=90:
+    print("Your grade is'A'")
+elif(mark>=80 and mark<90):
+    print("Your grade is 'B'")
+elif mark>=70 and mark<80:
+    print("Your grade is'C'")
+else:
+    print("Your grade is'D'")
