4520f9e42b249259afb24e4afcdd0fb39083ceaa
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-13 23:03:33 +0530

    Add files via upload

---
+++ Anannya Abhi/n_even_number.py
@@ -0,0 +1,16 @@
+'''
+Name: Anannnya Abhi
+Date: 13.11.24
+Python program to generate n even numbers
+'''
+limit=int(input("Enter value for n:"))
+even_num=-2
+even_num1=2
+if limit>=0:
+ for i in range(0,limit):
+  even_num=even_num+2
+  print(even_num,end=" ")
+else:
+    for i in range(limit,0):
+        even_num1 = even_num1 - 2
+        print(even_num1, end=" ")
