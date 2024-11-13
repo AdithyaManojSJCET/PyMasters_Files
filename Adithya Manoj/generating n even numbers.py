90ec13344c6a5fdcc1f48f2fa79ba4f271299fc7
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2024-11-13 23:26:19 +0530

    Add files via upload

---
+++ Adithya Manoj/generating n even numbers.py
@@ -0,0 +1,9 @@
+'''
+Name: Adithya Manoj
+Date: 13/11/2024
+Python prgram to generate N even numbers
+'''
+n=int(input("Enter the Limit"))
+
+even_numbers=list(range(2,(n*2)+1,2))
+print(even_numbers)
