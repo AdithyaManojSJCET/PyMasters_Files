7d9076f19f2d22b72e171df156b915b0b0203094
Ananthakrishnan K V <ananthakrishnankv2028@cs.sjcetpalai.ac.in>
2024-11-13 21:36:20 +0530

    Add files via upload

---
+++ AnanthaKrishna/Multiples of 5.py
@@ -0,0 +1,8 @@
+'''Auther : ananthakrishnan K V
+Date : 11-11-2024
+ Program to print first 10  multiples of 5 
+'''
+
+for i in range(1,11):
+    mult=5*i
+    print(f"5*{i}={mult}")
---
+++ AnanthaKrishna/even numbers.py
@@ -0,0 +1,7 @@
+'''Auther : ananthakrishnan K V
+Date : 11-11-2024
+ Program to print even numbers from 2 to 20
+'''
+
+for i in range(2,21,2):
+    print(i)
