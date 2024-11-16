7bd359e541d769161c5866a92a291c4e9cef4fcc
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-16 23:03:52 +0530

    Add files via upload

---
+++ Anannya Abhi/list_fruits.py
@@ -0,0 +1,8 @@
+'''
+Name: Anannnya Abhi
+Date: 16.11.24
+Python program to create list and add element using append()
+'''
+fruits=['Apples','Grapes','Bananas']
+fruits.append('Watermelon')
+print(fruits)
