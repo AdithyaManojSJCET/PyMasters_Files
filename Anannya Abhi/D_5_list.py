ccadc3227a06f4162ddc71bdeda9439cef9d1502
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-16 22:54:49 +0530

    Add files via upload

---
+++ Anannya Abhi/D_5_list.py
@@ -0,0 +1,7 @@
+'''
+Name: Anannnya Abhi
+Date: 16.11.24
+Python program to create list with numbers & to print first & last elements
+'''
+numbers=[1,2,3,4,5]
+print(f"The first number is:{numbers[0]}\nThe last number is:{numbers[-1]}")
