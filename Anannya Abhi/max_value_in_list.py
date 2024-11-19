4ece15b4961c2e07b9f3e950553905cf255152ad
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-19 23:11:32 +0530

    Add files via upload

---
+++ Anannya Abhi/max_value_in_list.py
@@ -0,0 +1,11 @@
+'''
+Name: Anannnya Abhi
+Date: 19.11.24
+Python program to find max value in a list
+'''
+lst=[]
+limit=int(input("Enter the limit:"))
+for i in range(0,limit):
+    number=int(input("Enter the number:"))
+    lst.append(number)
+print(f"The maximum value in the list is : {max(lst)}")
