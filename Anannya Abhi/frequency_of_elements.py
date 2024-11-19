c63ef9a2eff3e76dd7984d63621c04da32a4f750
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-19 23:06:54 +0530

    Add files via upload

---
+++ Anannya Abhi/frequency_of_elements.py
@@ -0,0 +1,11 @@
+'''
+Name: Anannnya Abhi
+Date: 19.11.24
+Python program to get the frequency of elements in list
+'''
+list=[]
+num=int(input("Enter the number of list elements: "))
+for i in range(num):
+    element=int(input("Enter the element:"))
+    list.append(element)
+    print(f"The frequency of {element} is : {list.count(element)}")
