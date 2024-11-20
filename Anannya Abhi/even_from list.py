39b52e0cb2326f9f3bab7edbec56007c6362bd4a
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-20 22:47:20 +0530

    Add files via upload

---
+++ Anannya Abhi/even_from list.py
@@ -0,0 +1,19 @@
+'''
+Name: Anannnya Abhi
+Date: 20.11.24
+Python program to find even numbers from a list using for loop
+'''
+limit=int(input("Enter the limit:"))
+lst=[]
+for i in range(0,limit):
+    element=int(input("Enter the element:"))
+    lst.append(element)
+for i in lst:
+    if i%2==0:
+        print(f"The given number {i} is even.")
+    else:
+        print(f"The given number {i} is odd.")
+
+
+
+
