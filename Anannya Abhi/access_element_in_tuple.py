e339107df8218cf51122caa66c753d6e78a863c3
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-21 23:23:57 +0530

    Add files via upload

---
+++ Anannya Abhi/access_element_in_tuple.py
@@ -0,0 +1,8 @@
+'''
+Name: Anannya Abhi
+Date: 21.11.24
+Python program to accessing elements in a tuple
+'''
+food=("Burger","Sandwich","Pizza","Biriyani")
+print(f"The last element in the tuple is: {food[-1]}")
+print(f"The first element in the tuple is: {food[0]}")
