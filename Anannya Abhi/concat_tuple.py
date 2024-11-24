8e533d6698d0b3ee953d7cc46f14a435e43cba80
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-24 22:53:03 +0530

    Add files via upload

---
+++ Anannya Abhi/concat_tuple.py
@@ -0,0 +1,9 @@
+'''
+Name: Anannya Abhi
+Date: 24.11.24
+Python program to concatenate two tuples
+'''
+name=("Tio","Leo",'Rio','Jio')
+occupation=("Teacher",'Doctor','Designer','Lawyer')
+details=name+occupation
+print(f"The concatenated tuple is: {details}")
