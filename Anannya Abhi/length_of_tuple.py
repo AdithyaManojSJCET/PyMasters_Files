dc7e99c0821a802361e97c9cc555cf8bbb4630ca
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-24 22:52:41 +0530

    Add files via upload

---
+++ Anannya Abhi/length_of_tuple.py
@@ -0,0 +1,7 @@
+'''
+Name: Anannya Abhi
+Date: 24.11.24
+Python program to find the length of a tuple
+'''
+name=("Tio","Leo",'Rio','Jio','Kio','Mio','Nio','Vio')
+print(f"The length of the tuple is: {len(name)}")
