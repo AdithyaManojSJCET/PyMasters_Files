69db64f77a4c53509a1b3dd8c1412c5613be2265
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-24 22:55:28 +0530

    Add files via upload

---
+++ Anannya Abhi/check_element.py
@@ -0,0 +1,10 @@
+'''
+Name: Anannya Abhi
+Date: 24.11.24
+Python program to check if an element exists in a tuple
+'''
+name=("Tio","Leo",'Rio','Jio')
+if "Tio"in name:
+    print("Tio in tuple!")
+else:
+    print("Tio not in tuple..")
