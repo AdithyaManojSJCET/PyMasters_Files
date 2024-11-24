58de55652fdf227ecdbc69a58e0a63eb4fcdc673
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-24 22:54:29 +0530

    Add files via upload

---
+++ Anannya Abhi/print _all_elements.py
@@ -0,0 +1,8 @@
+'''
+Name: Anannya Abhi
+Date: 24.11.24
+Python program to print each elem
+'''
+elements=(1,12,123,1234,12345,123456)
+for i in range(0,len(elements)):
+    print(f"Element at position {i+1} in tuple is: {elements[i]}")
