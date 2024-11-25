07513ebc01f425ee635d2c02d60f26964a3e0133
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-25 22:50:21 +0530

    Add files via upload

---
+++ Anannya Abhi/min_max_tuple.py
@@ -0,0 +1,8 @@
+'''
+Name: Anannya Abhi
+Date: 25.11.24
+Python program to find min and max in a tuple
+'''
+lst=(12,223,234,245,677)
+print(f"The minimum value in the tuple is: {min(lst)}"
+      f"\nThe maximum value in the tuple is: {max(lst)}")
