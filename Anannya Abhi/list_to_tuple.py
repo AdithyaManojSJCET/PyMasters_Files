cee4588b50dc89dc99dc70aa5baedf39d417ae66
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-25 22:48:59 +0530

    Add files via upload

---
+++ Anannya Abhi/list_to_tuple.py
@@ -0,0 +1,8 @@
+'''
+Name: Anannya Abhi
+Date: 25.11.24
+Python program to convert list to tuple
+'''
+lst=["Tomato","Cucumber",'Avocado']
+fruits=tuple(lst)
+print(f"The converted tuple is: {fruits}")
