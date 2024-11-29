83457aba7090c3da93815e16efd9c28ad86bd327
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-29 23:08:50 +0530

    Add files via upload

---
+++ Anannya Abhi/remove_from_dictionary.py
@@ -0,0 +1,9 @@
+'''
+Name: Anannya Abhi
+Date: 29.11.24
+Python program to remove a key from dictionary
+'''
+values={'name': 'Pooja', 'age': 30, 'job': 'Doctor'}
+print(f"The original dictionary is: {values}")
+del values["job"]
+print(f"The dictionary after removing a key is: {values}")
