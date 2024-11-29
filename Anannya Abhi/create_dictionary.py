4440a0aad75534fcfdda5ae3bad629d83dd107ed
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-29 23:08:04 +0530

    Add files via upload

---
+++ Anannya Abhi/create_dictionary.py
@@ -0,0 +1,9 @@
+'''
+Name: Anannya Abhi
+Date: 29.11.24
+Python program to create a dictionary with the following key-value pairs
+'''
+values={'name': 'Raju','age': 25,'city': 'Delhi'}
+print(f"The name stored is: {values['name']}")
+print(f"Age is: {values['age']}")
+print(f"City is: {values['city']}")
