81b07e1451ad18319fe8fbd4f7d60e2180aceea3
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-29 23:08:28 +0530

    Add files via upload

---
+++ Anannya Abhi/add_in_dictionary.py
@@ -0,0 +1,9 @@
+'''
+Name: Anannya Abhi
+Date: 29.11.24
+Python program to add new key-value pair
+'''
+values={'fruit': 'apple', 'color': 'red'}
+print(f"The original dictionary is: {values}")
+values.update({'price':10})
+print(f"The dictionary after adding a new key is: {values}")
