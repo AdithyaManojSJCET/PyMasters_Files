02e243b619ac165d378ae5a9f60d4c8b1cec2e7f
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-30 22:52:20 +0530

    Add files via upload

---
+++ Anannya Abhi/update_dictionary.py
@@ -0,0 +1,8 @@
+'''
+Name: Anannya Abhi
+Date: 30.11.24
+Python program to update a dictionary
+'''
+elements={'name': 'Pooja', 'age': 30, 'job': 'Doctor','salary':600000}
+elements.update({'salary':675000})
+print(f"The updated dictionary is: {elements}")
