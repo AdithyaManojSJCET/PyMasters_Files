79f42548cfff66051668efe7356dbf7eeb948618
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-30 22:51:53 +0530

    Add files via upload

---
+++ Anannya Abhi/retrieve_from_dictionary.py
@@ -0,0 +1,9 @@
+'''
+Name: Anannya Abhi
+Date: 30.11.24
+Python program to retrieve keys,values,items from dictionary
+'''
+elements={'name': 'Pooja', 'age': 30, 'job': 'Doctor'}
+print(f'The keys in the dictionary are: {elements.keys()}')
+print(f"The values in the dictionary are: {elements.values()}")
+print(f"The items in the dictionary are:{elements.items()}")
