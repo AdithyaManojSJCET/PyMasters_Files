4f4a747339a74b6bfaaf389bd7ab10a3a71ad289
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-30 22:51:11 +0530

    Add files via upload

---
+++ Anannya Abhi/length_of_dictionary.py
@@ -0,0 +1,7 @@
+'''
+Name: Anannya Abhi
+Date: 30.11.24
+Python program to find length of dictionary
+'''
+values={'name': 'Pooja', 'age': 30, 'job': 'Doctor'}
+print(f"The length of dictionary is: {len(values)}")
