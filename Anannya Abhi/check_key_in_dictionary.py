c9a66cdb04d04c7019f7a906c794e73bd33fc0f8
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-12-01 23:00:49 +0530

    Add files via upload

---
+++ Anannya Abhi/check_key_in_dictionary.py
@@ -0,0 +1,8 @@
+'''
+Name: Anannya Abhi
+Date: 1.12.24
+Python program to Check Key Existence in dictionary
+'''
+values={'fruit': 'apple', 'color': 'red','name': 'Raju','age': 25,'city': 'Delhi'}
+if 'color' in values:
+    print("Color is in the dictionary")
