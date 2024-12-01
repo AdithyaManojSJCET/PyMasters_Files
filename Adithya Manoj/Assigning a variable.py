0c719b298ee1dd18cb367a2e9eaea43833e9968d
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2024-12-02 03:23:45 +0530

    Delete Adithya Manoj directory

--- Adithya Manoj/Assigning a variable.py
+++
@@ -1,9 +0,0 @@
-'''
-Name: Adithya Manoj
-Date: 10/11/2024
-Assigning variables with characters, integer and float (decimal) values
-'''
-a=123
-b="Adithya"
-c=12.34
-d="Adithya123 12.34"
--- Adithya Manoj/Multiples of 5.py
+++
@@ -1,7 +0,0 @@
-'''
-Author: Adithya Manoj
-Date: 12/11/2024
-Print Multiples of 5
-'''
-for i in range(1,11):
-    print(f"5 * {i}\t={5*i}")
