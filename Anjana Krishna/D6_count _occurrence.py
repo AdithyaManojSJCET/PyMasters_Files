33e5f0477931dceb40e78d52b8fd9f7d0c031d52
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-12-14 19:31:00 +0530

    Add files via upload

---
+++ Anjana Krishna/D6_count _occurrence.py
@@ -0,0 +1,8 @@
+'''Anjana Krishna
+   Date 18 Nov 2024
+   Program to count the occurrence of an element'''
+Chocolates=['Mars','Twix','Galaxy','Kit-Kat','Mars','Twix','Kit-Kat','Twix']
+print(Chocolates.count('Mars'))
+print(Chocolates.count('Twix'))
+print(Chocolates.count('Galaxy'))
+print(Chocolates.count('Kit-Kat'))
