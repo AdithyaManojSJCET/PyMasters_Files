90af6cbd510f6065db246c23a802ca46e1aa5d66
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 10:11:41 +0530

    Add files via upload

---
+++ Anjana Krishna/unique_element.py
@@ -0,0 +1,9 @@
+
+original_list=[1,2,3,3,4]
+unique_element=[]
+for number in  original_list:
+      if number not in unique_element:
+          unique_element.append(number)
+print("Unique value:",unique_element)
+          
+      
