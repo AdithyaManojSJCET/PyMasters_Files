d1e10b826ac8ed22edf3b81941bba8c1e0b8d140
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 16:58:24 +0530

    Add files via upload

---
+++ Anjana Krishna/concatenate_tupple.py
@@ -0,0 +1,12 @@
+
+'''Author Anjana Krishna
+ Day 10 Python Challenge
+ Program to concatenate two tupple
+ '''
+
+first=(1,6,7)
+second=(9,5,4,8)
+concatenated_tupple=first+second
+print("The combind tupple is:",concatenated_tupple)
+
+
