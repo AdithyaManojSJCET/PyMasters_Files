c9d294ef33559ef0e2ebe95b4f8674801c3302f1
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 10:37:05 +0530

    Add files via upload

---
+++ Anjana Krishna/D7 string_to_list.py
@@ -0,0 +1,8 @@
+''' Author Anjana Krishna
+   Day 7 Python Challenge
+   Program to convert a string to a list
+   '''
+   string=input("Enter a string:")
+   
+   converted_string= list.(string)
+   print(converted_string)
