f8839b7f1789f7e198f23354c7a3621407b15363
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 16:21:02 +0530

    Add files via upload

---
+++ Anjana Krishna/D9_tupple.py
@@ -0,0 +1,16 @@
+'''Author Anjana Krishna
+  Day 9 Python Challenge
+  Programe to create & display,access element and to unpack a tupple
+  '''
+  
+Pets=("Max","Cocoa","Oreo")
+print(Pets)
+#accessing an element
+print(Pets[0])
+print(Pets[1])
+print(Pets[2])
+#unpacking tupple
+Pet1,Pet2,Pet3=Pets
+print(Pet1)
+print(Pet2)
+print(Pet3)
