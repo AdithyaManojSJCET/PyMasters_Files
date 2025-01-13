91b26cd68c681d35c68669322c8b66cd917c2928
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 20:42:35 +0530

    Add files via upload

---
+++ Anjana Krishna/D12__tuple.py
@@ -0,0 +1,14 @@
+'''Author Anjana Krishna
+Day 12 Python Challenge
+Program with tuple
+'''
+#coverting a list to tuple
+list=[2,4,6,8,10,4,8,4]
+converted_list=tuple(list)
+print(converted_list)
+#occurance of an element
+print(converted_list.count(4))
+
+#maximum and minimum value
+print(max(converted_list))
+print(min( converted_list))
