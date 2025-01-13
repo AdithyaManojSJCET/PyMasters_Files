46c61097bddacb594b1bbd1cbc51b346b4483910
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 19:41:38 +0530

    length_of_tupple.py to length_of_tuple.py

---
+++ Anjana Krishna/length_of_tuple.py
@@ -0,0 +1,6 @@
+'''Author Anjana Krishna
+  Day 10 Python Challenge
+  Program to find the length of tuple
+  '''
+tuple=(10,20,30,40)
+print("Lenght of tuple:",len(tuple))
--- Anjana Krishna/length_of_tupple.py
+++
@@ -1,6 +0,0 @@
-'''Author Anjana Krishna
-  Day 10 Python Challenge
-  Program to find the length of tupple
-  '''
-tupple=(10,20,30,40)
-print("Lenght of tupple:",len(tupple))
