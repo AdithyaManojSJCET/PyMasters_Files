dfe962a9bc4eac4963cb928acc86d3682e3c602b
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 19:06:27 +0530

    Add files via upload

---
+++ Anjana Krishna/D11_tupple.py
@@ -0,0 +1,18 @@
+''' Author Anjana Krishna
+ Day 11Python Challenge
+ Program with tupple 
+ '''
+ 
+#To access 3rd element
+Books=("The Alchemist","Emma","The Blue Umbrella","Wings of Fire")
+print(Books[2])
+
+#To check an element exist in a tuple
+print("The Alchemist"in Books)
+print("Harry Potter"in Books)
+
+#To print each element seperately
+print(Books[0])
+print(Books[1])
+print(Books[2])
+print(Books[3])
