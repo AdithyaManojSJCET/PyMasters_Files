8e6d9e0e027cc407c004b24ef4d546a38827325b
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-13 10:44:05 +0530

    Add files via upload

---
+++ Anjana Krishna/Max_value_list.py
@@ -0,0 +1,7 @@
+'''Author Anjana Krishna
+  Day 7 Python Challenge
+  Program to find maximum from a list
+  '''
+  list=[10,20,5,8]
+  sorted_list=list.sort()
+  print("Maximum value:",sorted_list=[-1])
