b8df9e24a2cef04da485baa6b7316a2ba15a2dea
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-14 18:19:53 +0530

    Add files via upload

---
+++ Anjana Krishna/D15_set.py
@@ -0,0 +1,27 @@
+'''Author Anjana Krishna
+ Day15 Python Challwnge
+ Program with set 
+ '''
+Grocery={"Milk","Bread","Eggs","Avacado"}
+
+#add()
+Grocery.add("Tomato")
+print(Grocery)
+ 
+#update()
+Grocery.update(["Onion","Flour"])
+print(Grocery)
+
+
+#discard
+Grocery.discard("Milk")
+print(Grocery)
+
+
+#pop
+Grocery.pop()
+print(Grocery)
+
+#clear
+Grocery.clear()
+print(Grocery)
