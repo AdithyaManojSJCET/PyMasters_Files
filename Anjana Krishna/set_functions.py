618801259733b6516a30228e805b2dfdfd0f0661
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-14 18:04:19 +0530

    Add files via upload

---
+++ Anjana Krishna/set_functions.py
@@ -0,0 +1,17 @@
+'''Author Anjana Krishna
+ Day 14 Python Challenge
+ Program with set functions
+ '''
+A={10,20,30}
+B={30,40,50,60}
+#union
+union=A|B
+print("Union :",union)
+
+#difference
+difference=A-B
+print("Difference:",difference)
+
+#intersection
+intersection=A.intersection(B)
+print("Intersection:",intersection)
