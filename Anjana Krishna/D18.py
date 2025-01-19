62ca1120f7d8ac9c34eded4c8300830c76ed0df2
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-19 22:57:06 +0530

    Add files via upload

---
+++ Anjana Krishna/D18.py
@@ -0,0 +1,17 @@
+
+'''Author Anjana Krishna
+  Day 18 Python Challenge
+'''
+
+my_dict={"name":"Alice","age":30,"place":"Kottayam"}
+#to remove using pop
+my_dict.pop("age")
+print(my_dict)
+
+#to clear
+print(my_dict.clear())
+
+
+#key existence
+print( "name" in my_dict)
+
